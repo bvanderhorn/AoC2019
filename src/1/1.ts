7a75f8429e175f67cc9606b609da726ef102204d
bvanderhorn <bvanderhorn@gmail.com>
2023-08-27 18:39:59 +0200

    chore: clearer

--- src/1/1.ts
+++ src/1/1.ts
@@ -1,10 +1,11 @@
 import * as h from '../helpers';
 
-var toFuel = (mass: number, part: number): number => {
-    var fuel = Math.floor(mass / 3) - 2;
-    return fuel <= 0 ? 0 : fuel + (part == 1 ? 0 : toFuel(fuel, part));
+var toFuel1 = (mass: number): number => Math.floor(mass / 3) - 2;
+var toFuel2 = (mass: number): number => {
+    var fuel = toFuel1(mass);
+    return fuel <= 0 ? 0 : fuel + toFuel2(fuel);
 }
 
 var modules = h.read(1, "modules.txt");
-h.print("part 1:", modules.map(x => toFuel(x, 1)).sum());
-h.print("part 2:", modules.map(x => toFuel(x, 2)).sum());
+h.print("part 1:", modules.map(toFuel1).sum());
+h.print("part 2:", modules.map(toFuel2).sum());
