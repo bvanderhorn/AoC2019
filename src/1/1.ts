c924dbb4256adce3034fd3a897abe1fa60a28446
bvanderhorn <bvanderhorn@gmail.com>
2023-08-27 18:38:41 +0200

    feat: day 1 part 2

--- src/1/1.ts
+++ src/1/1.ts
@@ -1,6 +1,10 @@
 import * as h from '../helpers';
 
-var toFuel = (mass: number): number => Math.floor(mass / 3) - 2;
+var toFuel = (mass: number, part: number): number => {
+    var fuel = Math.floor(mass / 3) - 2;
+    return fuel <= 0 ? 0 : fuel + (part == 1 ? 0 : toFuel(fuel, part));
+}
 
 var modules = h.read(1, "modules.txt");
-h.print("part 1:", modules.map(toFuel).sum());
+h.print("part 1:", modules.map(x => toFuel(x, 1)).sum());
+h.print("part 2:", modules.map(x => toFuel(x, 2)).sum());
