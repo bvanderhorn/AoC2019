f6a90f32b60566bb11658840c0a1882c24a1df2f
bvanderhorn <bvanderhorn@gmail.com>
2023-08-30 11:16:42 +0200

    feat: day 4 part 1

--- src/4/4.ts
+++ src/4/4.ts
@@ -1,5 +1,18 @@
 import * as h from '../helpers';
+var meets = (n: number) => {
+    var s = n.toString();
+    var containsDouble = false;
+    for (const i of h.range(1, s.length)){
+        if (s[i] < s[i-1]) return false;
+        if (s[i] == s[i-1]) containsDouble = true;
+    }
+    return containsDouble;
+}
 
+// execute
 var range = h.read(4, "range.txt")[0].split('-').tonum();
 
-h.print(range);
+// day 1: brute force
+var count = 0;
+for (const i of h.range(range[0], range[1]+1)) if (meets(i)) count++;
+h.print("part 1:", count);
