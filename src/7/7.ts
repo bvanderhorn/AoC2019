afa0a82849c5e972b6c472b696146790220ff11e
bvanderhorn <bvanderhorn@gmail.com>
2023-09-22 16:28:35 +0200

    feat: trying some stuff

--- src/7/7.ts
+++ src/7/7.ts
@@ -11,26 +11,23 @@ var run1 = (program: number[], input: number[] = []): number[] => {
     var index = {i:0};
     var halt = {h:false};
     var inputIndex = 0;
-    var output:number[] = [];
-    var ol = 0;
     while(true) {
+        var output:number[] = [];
         var consumed = ic.execute(program, index, halt, input[inputIndex], output);
         if (consumed) inputIndex = 1;
-        if (output.length > ol) {
-            ol = output.length;
-            input[1] = output.last();
+        if (output.length > 0) {
+            input[1] = output[0];
         }
         if (halt.h) return output;
     }
 }
 
-var program = h.read(7, "program.txt",)[0].split(',').tonum();
+var program = h.read(7, "program.txt", "ex")[0].split(',').tonum();
 var phases: number[][] = [0,1,2,3,4].permutations();
 h.print(phases.length);
 h.print("part 1:", phases.map(p => customRun(program.copy(), p, 1, ic.run)).max());
 
 // part 2
-var program2 = program.copy();
 var phases2: number[][] = [5,6,7,8,9].permutations();
 var allPhases: number[][] = phases.map(p => phases2.map(p2 => p.concat(p2))).flat();
 allPhases.slice(0,10).print();
