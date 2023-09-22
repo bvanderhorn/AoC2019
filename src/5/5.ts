f13f170ac92374d6a1db5b4cd867e7af2853c023
bvanderhorn <bvanderhorn@gmail.com>
2023-09-22 15:44:01 +0200

    chore: cleanup

--- src/5/5.ts
+++ src/5/5.ts
@@ -2,11 +2,5 @@ import * as h from '../helpers';
 import * as ic from '../intcode';
 
 var program = h.read(5, "program.txt",)[0].split(',').tonum();
-
-h.print("-- part 1 --")
-var index = {i:0};
-ic.run(program.copy(), [1]).print(' - ');
-
-h.print("-- part 2 --")
-index.i = 0;
-ic.run(program.copy(), [5]).print();
+h.print("part 1:", ic.run(program.copy(), [1]).string(' - '));
+h.print("part 2:", ic.run(program.copy(), [5]).string());
