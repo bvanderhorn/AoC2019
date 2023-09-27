7852decd6d6ba9c720f307c97bab2f84bff56aa0
bvanderhorn <bvanderhorn@gmail.com>
2023-09-27 15:34:23 +0200

    feat: day 9 pt 2

--- src/9/9.ts
+++ src/9/9.ts
@@ -3,6 +3,5 @@ import * as ic from '../intcode';
 
 var program = h.read(9, "program.txt")[0].split(',').tonum();
 
-var state = new ic.State(program.copy(), [1]);
-var output = state.run();
-h.print("part 1:", output.join(", "));
+h.print("part 1:", new ic.State(program.copy(), [1]).run().join(", "));
+h.print("part 2:", new ic.State(program.copy(), [2]).run().join(", "));
