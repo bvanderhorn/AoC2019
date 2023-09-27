5a1bb633f46e9e4f9ce945e3bec587b940cf92cb
bvanderhorn <bvanderhorn@gmail.com>
2023-09-27 13:27:04 +0200

    feat: progress

--- src/9/9.ts
+++ src/9/9.ts
@@ -1,2 +1,8 @@
 import * as h from '../helpers';
 import * as ic from '../intcode';
+
+var program = h.read(9, "program.txt","ex")[0].split(',').tonum();
+
+var state = new ic.State(program.copy());
+var output = state.run();
+h.print("part 1:", output.string());
