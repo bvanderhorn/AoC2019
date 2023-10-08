9e9c4435d988461177629b4d34bd2dc3f1b6a78a
bvanderhorn <bvanderhorn@gmail.com>
2023-10-08 11:22:45 +0200

    feat: day 14 pt 1

--- src/14/14.ts
+++ src/14/14.ts
@@ -1,3 +1,4 @@
+import { get } from 'http';
 import * as h from '../helpers';
 
 var dissect = (equation: string) : {amount: number, mineral: string} => {
@@ -6,30 +7,32 @@ var dissect = (equation: string) : {amount: number, mineral: string} => {
 }
 var getEquation = (mineral: string, equations: string[][]) : string[] => equations.filter(x => dissect(x[0]).mineral == mineral)[0];
 
-var getIntermediates = (fromMineral:string, equations: string[][], toMineral:string = "ORE") : string[] => {
+var getIntermediates = (fromMineral:string, equations: string[][], toMineral: string) : string[] => {
     var fromEq = getEquation(fromMineral, equations).map(x => dissect(x).mineral);
     var next = fromEq.slice(1).filter(x => x != toMineral);
     var tree = [...next];
-    for (const n of next) tree.push(...getIntermediates(n, equations));
+    for (const n of next) tree.push(...getIntermediates(n, equations, toMineral));
     return tree.unique();
 }
 
-var getNotInOtherIntermediates = (minerals: string[], equations: string[][]) : string[] => {
-    var intermediates = minerals.map(m => getIntermediates(m, equations));
+var getNotInOtherIntermediates = (minerals: string[], equations: string[][], toMineral: string) : string[] => {
+    var intermediates = minerals.map(m => getIntermediates(m, equations, toMineral));
     return minerals.filter((m,i) => !intermediates.some((x,j) => i != j && x.includes(m)));
 }
 
-var equations = h.read(14, "equations.txt", "ex").split(/=>|,/).trim().map(x => x.reverse());
+var equations = h.read(14, "equations.txt").split(/=>|,/).trim().map(x => x.reverse());
 // h.print(equations.slice(0,3));
 
 console.time("part 1");
-var fuelEq = getEquation("FUEL", equations);
+var fromMineral = "FUEL";
+var toMineral = "ORE";
+var fuelEq = getEquation(fromMineral, equations);
 var totalEquation = new Map<string, number>();
 fuelEq.slice(1).forEach(x => totalEquation.set(dissect(x).mineral, dissect(x).amount));
 
 // h.print(totalEquation);
 while(totalEquation.size >1) {
-    var next = getNotInOtherIntermediates(Array.from(totalEquation.keys()), equations);
+    var next = getNotInOtherIntermediates(Array.from(totalEquation.keys()).filter(x => x!= toMineral), equations, toMineral);
     for (const n of next){
         var eq = getEquation(n, equations);
         var amount = dissect(eq[0]).amount;
