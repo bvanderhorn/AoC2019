541d75ae8f133a8b637f32154cd63e4982f072f4
bvanderhorn <bvanderhorn@gmail.com>
2023-10-08 12:38:41 +0200

    feat: day 14 pt 2

--- src/14/14.ts
+++ src/14/14.ts
@@ -20,29 +20,89 @@ var getNotInOtherIntermediates = (minerals: string[], equations: string[][], toM
     return minerals.filter((m,i) => !intermediates.some((x,j) => i != j && x.includes(m)));
 }
 
-var equations = h.read(14, "equations.txt").split(/=>|,/).trim().map(x => x.reverse());
-// h.print(equations.slice(0,3));
+var equate = (equation: Map<string, number>, toMineral: string, equations: string[][], equateOrder: string[] = []) : {needed: number, equateOrder: string[]} => {
+    // if equate order is known, use to speed up the process
+    var totalEquation = new Map(equation);
+    var equateOrderOut: string[] = [];
+    while(totalEquation.size >1) {
+        var next = equateOrder.length == 0 
+            ? getNotInOtherIntermediates(Array.from(totalEquation.keys()).filter(x => x!= toMineral), equations, toMineral)
+            : equateOrder;
+        for (const n of next){
+            var eq = getEquation(n, equations);
+            var amount = dissect(eq[0]).amount;
+            var needed = totalEquation.get(n);
+            var times = Math.ceil((needed ?? 0) / amount);
+            for (const e of eq.slice(1).map(x => dissect(x))) {
+                totalEquation.set(e.mineral, (totalEquation.get(e.mineral) ?? 0) + e.amount * times);
+            }
+            totalEquation.delete(n);
+            equateOrderOut.push(n);
+        }    
+    }
+    return {
+        needed: totalEquation.get(toMineral) ?? 1E15,
+        equateOrder: equateOrderOut
+    };
+}
 
-console.time("part 1");
+var equationTimes = (amount: number, equation: Map<string, number> ) : Map<string, number> => 
+    new Map(Array.from(equation, ([k,v]) => [k, v * amount] as [string, number]));
+
+var equateTimes = (amount: number, equation: Map<string, number>, toMineral: string, equations: string[][], equateOrder: string[]) : number => {
+    var totalEquation = equationTimes(amount, equation);
+    return equate(totalEquation, toMineral, equations, equateOrder).needed;
+}
+
+var newtonSearch = (amount: number, singleNeeded: number, equation: Map<string, number>, toMineral: string, equations: string[][], equateOrder: string[]) : number => {
+    var times = Math.floor(amount / singleNeeded);
+    var deltaTimes = times;
+    
+    // initial search
+    while(deltaTimes > 1) {
+        var needed = equateTimes(times, equation, toMineral, equations, equateOrder);
+        if (needed > amount) {
+            times -= deltaTimes;
+            deltaTimes = Math.ceil(deltaTimes / 2);
+        } else {
+            times += deltaTimes;
+        }
+    }
+
+    // refined search
+    var curNeeded = equateTimes(times, equation, toMineral, equations, equateOrder);
+    if (curNeeded == amount) return times;
+    else if (curNeeded > amount) {
+        while (curNeeded > amount) {
+            times--;
+            curNeeded = equateTimes(times, equation, toMineral, equations, equateOrder);
+        }
+        return times;
+    }
+    else {
+        while (curNeeded < amount) {
+            times++;
+            curNeeded = equateTimes(times, equation, toMineral, equations, equateOrder);
+        }
+        return curNeeded == amount ? times : times - 1;
+    }
+}
+
+var equations = h.read(14, "equations.txt").split(/=>|,/).trim().map(x => x.reverse());
+
 var fromMineral = "FUEL";
 var toMineral = "ORE";
+
+// get main equation
 var fuelEq = getEquation(fromMineral, equations);
 var totalEquation = new Map<string, number>();
 fuelEq.slice(1).forEach(x => totalEquation.set(dissect(x).mineral, dissect(x).amount));
 
-// h.print(totalEquation);
-while(totalEquation.size >1) {
-    var next = getNotInOtherIntermediates(Array.from(totalEquation.keys()).filter(x => x!= toMineral), equations, toMineral);
-    for (const n of next){
-        var eq = getEquation(n, equations);
-        var amount = dissect(eq[0]).amount;
-        var needed = totalEquation.get(n);
-        var times = Math.ceil((needed ?? 0) / amount);
-        for (const e of eq.slice(1).map(x => dissect(x))) {
-            totalEquation.set(e.mineral, (totalEquation.get(e.mineral) ?? 0) + e.amount * times);
-        }
-        totalEquation.delete(n);
-    }    
-}
-console.timeEnd("part 1");
-h.print("part 1:",totalEquation.get("ORE"));
+// part 1
+var p1 = equate(totalEquation, toMineral, equations);
+h.print("part 1:",p1.needed);
+
+// part 2
+var ore = 1E12;
+var times = newtonSearch(ore, p1.needed, totalEquation, toMineral, equations, p1.equateOrder);
+h.print("part 2:", times);
