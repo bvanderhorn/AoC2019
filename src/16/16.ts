1811a160c819428037d5d6a8c25803664d7d19b9
bvanderhorn <bvanderhorn@gmail.com>
2023-10-10 11:59:41 +0200

    feat: progress

--- src/16/16.ts
+++ src/16/16.ts
@@ -16,7 +16,7 @@ var mod = (n:number) : void => {
     h.print("pattern ",n,":", l, "%", patternLength, "=>", modd,";", )
 }
 
-var sequence = h.read(16, "sequence.txt", "ex")[0].split('').tonum();
+var sequence = h.read(16, "sequence.txt")[0].split('').tonum();
 
 var allPatterns = getAllPatterns(sequence.length);
 h.print("sequence:", sequence.join(''));
@@ -24,23 +24,19 @@ var phases = 100;
 
 // part 1
 var s1 = sequence.copy(); 
-// for (var i=0;i<phases;i++) {
-//     s1 = applyPatterns(s1, allPatterns);
-//     h.print("after", i+1, ":", s1.join(''));
-// }
+for (var i=0;i<phases;i++) {
+    s1 = applyPatterns(s1, allPatterns);
+    // h.print("after", i+1, ":", s1.join(''));
+}
 h.print("part 1:", s1.slice(0,8).join(''));
 
 // part 2: piece-wise
 const s09 = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 const n09 = h.range(0,10);
 // h.print(s09, "\n", n09);
-h.print((-1)%10);
 const s2nMap = new Map<string, number>(s09.map((x,i) => [x, n09[i]]));
 var s2n = (s:string) : number => s2nMap.get(s) ?? -1;
-var n2s = (n:number) : string => {
-    if (n<0) throw new Error(`Invalid index: ${n}`);
-    return s09[n%10];
-}
+var n2s = (n:number) : string => s09[Math.abs(n)%10];
 var getFromPattern = (n:number, index:number) : number => {
     var start = n-1;
     if (index < start) return 0;
@@ -53,4 +49,9 @@ var calculateIndex = (sequence:string[], index:number) : string => {
     for (var i=0;i<sequence.length; i++) result += getFromPattern(n,i)*s2n(sequence[i]); 
     return n2s(result);
 }
+var nextPhase = (curPhase:string[]) : string[] => Array.apply(null, Array(curPhase.length)).map((_,i) => calculateIndex(curPhase,i));
 
+// piece-wise part 1
+var s1a = sequence.copy();
+for (var i=0;i<phases;i++) s1a = nextPhase(s1a);
+h.print("piece-wise part 1:", s1a.slice(0,8).join(''));
