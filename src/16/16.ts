71a0c6dd8661eedf4559e156eec81a7fd6bb4cee
bvanderhorn <bvanderhorn@gmail.com>
2023-10-11 16:43:26 +0200

    feat: fromIndex

--- src/16/16.ts
+++ src/16/16.ts
@@ -1,6 +1,6 @@
 import * as h from '../helpers';
 
-var calculateIndex = (sequence:number[], index:number) : number => {
+var calculateToIndex = (sequence:number[], index:number) : number => {
     var result = 0;
     var curP = 1;
     var iP = 0;
@@ -19,10 +19,20 @@ var calculateIndex = (sequence:number[], index:number) : number => {
     return Math.abs(result)%10;
 }
 var nextPhase = (curPhase:number[], out:number[]) : void => {
-    for (var i=0;i<curPhase.length; i++) out[i] = calculateIndex(curPhase,i);
+    for (var i=0;i<curPhase.length; i++) out[i] = calculateToIndex(curPhase,i);
 }
 
-var sequence = h.read(16, "sequence.txt")[0].split('').tonum();
+var pn = (i:number): number[] => Array(i+1).fill(0).map((_:number,n:number) => getFromPattern(n+1,i));
+var calculateFromIndex = (sequenceOnIndex: number, index: number, out:number[]) : void => {
+    for (var j=0; j<=index;j++) out[j] += getFromPattern(j+1,index)*sequenceOnIndex;
+}
+const basePattern = [1,0,-1,0];
+var getFromPattern = (n:number, index:number) : number => {
+    var ims = index-n+1;
+    return ims<0 ? 0 : basePattern[Math.floor(ims/n) % 4];
+}
+
+var sequence = h.read(16, "sequence.txt", "ex")[0].split('').tonum();
 var phases = 100;
 
 // part 1
@@ -36,13 +46,28 @@ for (var j=0;j<phases;j++) {
 console.timeEnd("part 1");
 h.print("piece-wise part 1:", (phases%2===0 ? s1 : s1a).slice(0,8).join(''));
 
+// for (const i of h.range(0,50)) h.print("pattern", i, ":", JSON.stringify(pn(i)));
+
+// console.time("maxPattern");
+// var maxPattern = pn(65E5);
+// console.timeEnd("maxPattern");
+// console.time("minPattern");
+// var minPattern = pn(0);
+// console.timeEnd("minPattern");
 // part 2
 var s2Set = sequence.copy();
 var times = 1E4;
-var s2 = Array(s2Set.length * times).fill(0);
+var s2 : number[]= Array(s2Set.length * times).fill(0);
 s2 = s2.map((_,i) => s2Set[i%s2Set.length]);
 
-var n = 100;
-console.time(`pt 2 first ${n} indices`);
-for (var i=0;i<n;i++) calculateIndex(s2, i);
-console.timeEnd(`pt 2 first ${n} indices`);
+// var n = 100;
+// console.time(`pt 2 first ${n} indices`);
+// for (var i=0;i<n;i++) calculateToIndex(s2, i); 
+// console.timeEnd(`pt 2 first ${n} indices`);
+
+// test calculateFromIndex
+var s2a = Array(s2Set.length * times).fill(0);
+var imax = 1E4;
+console.time("fromIndex");
+for (var i=0;i<imax;i++) calculateFromIndex(s2[i],i,s2a);
+console.timeEnd("fromIndex");
